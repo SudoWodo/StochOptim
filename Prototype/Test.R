5356a634ee6ad3451274111e048f016c731f7235
Siddhant Raghuvanshi <82159869+SudoWodo@users.noreply.github.com>
2022-06-08 23:41:25 +0530

    upper / lower case change

--- Prototype/Test.R
+++ Prototype/Test.R
@@ -1,15 +1,16 @@
+D = 10
 # - with invalid function
-final_ans <- globalwrapper(fn = D, lower = lb, upper = ub, 
+final_ans <- GlobalWrapper(fn = D, lower = lb, upper = ub, 
                            method = "DEoptim", control = ctrl)
 
-final_ans <- globalwrapper(fn = NULL, lower = lb, upper = ub, 
+final_ans <- GlobalWrapper(fn = NULL, lower = lb, upper = ub, 
                            method = "DEoptim", control = ctrl)
 
 # - with invalid method
-final_ans <- globalwrapper(fn = rastrigin, lower = lb, upper = ub, 
+final_ans <- GlobalWrapper(fn = rastrigin, lower = lb, upper = ub, 
                            method = "Not a method", control = ctrl)
 
-final_ans <- globalwrapper(fn = rastrigin, lower = lb, upper = ub, 
+final_ans <- GlobalWrapper(fn = rastrigin, lower = lb, upper = ub, 
                            method = NULL, control = ctrl)
 
 # - with invalid control
@@ -19,7 +20,7 @@ ctrl1 <- list(NP      = 10*length(lb),
               reltol  = 1e-10, 
               tracee   = FALSE) # trace misspelled
 
-final_ans <- globalwrapper(fn      = rastrigin, 
+final_ans <- GlobalWrapper(fn      = rastrigin, 
                            lower   = lb,
                            upper   = ub, 
                            method  = "DEoptim", 
