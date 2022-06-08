71fb74182d923320f2682e6226ce39af150d9d5e
Siddhant Raghuvanshi <82159869+SudoWodo@users.noreply.github.com>
2022-06-08 14:38:01 +0530

    Updated user workflow

--- Prototype/UserWorkFlow.R
+++ Prototype/UserWorkFlow.R
@@ -2,8 +2,8 @@
 
 # Set working directory to folder containing this file
 
-# just for trying things out 
-# package does not use library and source
+# The scheme for the proposed package has been laid out
+# in a script like manner
 
 library(R6)
 source("GlobalWrapper.R")
@@ -23,11 +23,20 @@ D <- 10
 lb <- rep(-5.2, D)
 ub <- rep(5.2, D)
 
+
+
+## NOTE : Global wrapper returns the object which is provided by optimizer used.
+## NOTE : Global wrapper just prints the output in fashion similar to optimx
+
+
+# DEoptim --------------------------------------
+
 ctrl1 <- list(NP      = 10*length(lb),
               itermax = 200*length(lb),
               reltol  = 1e-10, 
               trace   = FALSE)
 
+set.seed(123)
 final_ans <- globalwrapper(fn      = rastrigin, 
                            lower   = lb,
                            upper   = ub, 
@@ -51,7 +60,6 @@ final_ans2 <- globalwrapper(par     = rep(1,D),
 
 summary(final_ans)
 
-
 ## GenSA ------------------------------------
 
 # GenSA will stop after finding the targeted function value 0 with
@@ -59,15 +67,16 @@ summary(final_ans)
 
 global_min <- 0
 tol <- 1e-13
-ctrl3 <- list(threshold.stop = global_min + tol)
+ctrl3 <- list(threshold.stop = global_min + tol, trace.mat = FALSE)
 
-final_ans3 <- globalwrapper(fn = rastrigin,
-                            lower = lower,
-                            upper = upper,
-                            method = "GenSA",
-                            control= ctrl3
-                            )
-summary(final_ans3)
+final_ans3 <- globalwrapper(fn      = rastrigin,
+                            lower   = lb,
+                            upper   = ub,
+                            method  = "GenSA",
+                            control = ctrl3)
+
+# note GenSA returns almost similar output as printed by global wrapper
+final_ans3
 
 
 
@@ -75,45 +84,31 @@ summary(final_ans3)
 
 # Rough work --------------------------------
 
+# DEoptim_control <- c("VTR","strategy","bs","NP","itermax","CR","F","trace",
+#                      "initialpop","storepopfrom","storepopfreq","p","c","reltol",
+#                      "steptol","parallelType","cluster","packages","parVar","foreachArgs")
+# 
+# 
+# myctrl <- list("bss","NP","itermax","CR")
+# 
+# myctrl %in% DEoptim_control
+# 
+# library(DEoptim)
+# formals(DEoptim)
+# formals(DEoptim.control)
+# 
+# library(rlang)
+# list(fn_fmls(DEoptim.control))
 
+# optim(par = rep(1,D), fn  = rastrigin, method = "Nelder")
 
-
-# PSO
-## Rastrigin function - local refinement with L-BFGS-B on improvements
 # psoptim(rep(NA,2),rastrigin,
-#         lower=-5,upper=5,control=list(abstol=1e-8,hybrid="improved"))
+#        lower=-5,upper=5,control=list(abstol=1e-8,hybrid="improved"))
 # 
-# par <- rep(NA, D)
-# fn <- rastrigin
-# lower <- rep(-5,D)
-# upper <- rep(5,D)
-# control = ctrl3
-# method = "pso"
-# 
-# localwrapperpso(par = par,
-#                 fn = rastrigin, 
-#                 lower = lb,
-#                 upper = ub, 
-#                 method = "pso",
-#                 control = ctrl3)
-# 
-# pso::psoptim(par     = par,
-#              fn      = rastrigin,
-#              lower   = lower,
-#              upper   = upper,
-#              control = ctrl3
-# )
-# 
-# psoptim(par = rep(NA,D),
-#         fn = rastrigin, 
-#         lower = lb,
-#         upper = ub, 
-#         control = ctrl3)
-# 
-# ##################################################
-# 
-# 
+# dimension <- 30
+# global.min <- 0
+# tol <- 1e-13
+# lower <- rep(-5.12, dimension)
+# upper <- rep(5.12, dimension)
 # out <- GenSA(lower = lower, upper = upper, fn = rastrigin,
-#              control=list(max.time=2))
-# out[c("value","par","counts")]
-
+#              control=list(threshold.stop=global.min+tol,verbose=TRUE))
